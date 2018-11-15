26f11fd201dbbd9343e80f636ec57fc1abc2010d
github username <github email address>
2018-11-15 16:48:56 -0700

    add navbar new schedule button

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Schedule from './components/Schedule'
 import PieChart from './components/PieChart'
 import NewSchedule from './components/NewSchedule'
 import ActivityForm from './components/ActivityForm'
-import Navbar from './components/Navbar'
+import MyNavbar from './components/Navbar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <Navbar />
+          <MyNavbar />
         </div>
         <ActivityForm />
         <NewSchedule />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Navbar, Nav, NavItem, MenuItem, NavDropdown, FormGroup, FormControl, Button } from 'react-bootstrap'
-
+import NewSchedule from './NewSchedule'
 class MyNavbar extends Component {
 
 	render() {
@@ -8,11 +8,16 @@ class MyNavbar extends Component {
       <Navbar>
         <Navbar.Header>
           <Navbar.Brand>
-            <a href="#home">Brand</a>
+            <a href="#home">Schedule-Life-Balance</a>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
+          <Navbar.Form pullLeft>
+            <FormGroup>
+              <NewSchedule />
+            </FormGroup>{' '}
+          </Navbar.Form>
           <Navbar.Form pullLeft>
             <FormGroup>
               <FormControl type="text" placeholder="Search" />
